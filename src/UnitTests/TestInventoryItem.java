112bb1c9cc013b26020699b4263f85f8dd7da443
Kevin Booth <kdbooth97@gmail.com>
2019-04-11 15:17:13 -0400

    Create midterm project, convert to maven, and add classes

---
+++ src/UnitTests/TestInventoryItem.java
@@ -0,0 +1,13 @@
+package UnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class TestInventoryItem {
+
+}
