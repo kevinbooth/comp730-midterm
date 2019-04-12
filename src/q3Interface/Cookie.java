1a448ab4265a3ee38b10c326a9b272f5ff82b6c5
Kevin Booth <kdbooth97@gmail.com>
2019-04-11 21:17:22 -0400

    Add Cookie class

--- src/q3Interface/Cookie.java
+++ src/q3Interface/Cookie.java
@@ -1,5 +1,23 @@
 package q3Interface;
 
-public class Cookie {
+public class Cookie implements BakedGoods {
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSellByDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
