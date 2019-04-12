d711c2d0398b695fdcec28f81d1dc3f5320ab60c
Kevin Booth <kdbooth97@gmail.com>
2019-04-11 21:17:53 -0400

    Add CinnamonRoll class

---
+++ src/q3Interface/CinnamonRoll.java
@@ -0,0 +1,23 @@
+package q3Interface;
+
+public class CinnamonRoll implements BakedGoods {
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSellByDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
